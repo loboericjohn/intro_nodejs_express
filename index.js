66f48da4fdae8c1a48f0bdfcb0ff4a637463d2f7
unknown <YY529@ccs.local>
2025-02-08 00:43:53 -0800

    commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send('Received: ${JSON.stringyfy(data)}');
+});
+app.use((req, res, next) => {
+    console.log('${req.method} ${req.url}');
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
